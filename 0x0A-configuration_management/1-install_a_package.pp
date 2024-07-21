0a37d1955ea38e32242293a9a8d14af27923a840
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-07-21 14:57:36 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Installs puppet-lint
 
+package { 'Werkzeug':
+  ensure   => '2.0.3',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['Werkzeug'],
 }
